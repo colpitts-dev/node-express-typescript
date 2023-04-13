ca2e337ad8e0217870fb962ce5e01cf587bb73e0
acolpitts <adam@colpitts.dev>
2023-04-12 23:35:22 -0300

    fix: remove test router import

--- src/routes/v1/index.router.ts
+++ src/routes/v1/index.router.ts
@@ -3,7 +3,6 @@ import { Router } from 'express'
 import { getIndex } from './index.controller'
 import peopleRoutes from './people/people.router'
 /* RESTFUL_IMPORT */
-import environmentsRouter from './environments/environments.router';
 
 const indexRouter = Router()
 
@@ -12,6 +11,5 @@ indexRouter.get('/', getIndex)
 
 indexRouter.use('/people', peopleRoutes)
 /* RESTFUL_API */
-indexRouter.use('/environments', environmentsRouter)
 
 export default indexRouter
